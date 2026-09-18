3a9366091c4a19b11c274876dbe9409f1a1f382b
Himanshu <himanshima@myvuw.ac.nz>
2026-09-18 19:20:02 +1200

    chore: initial commit

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import torch
+import torchvision.transforms as transforms
+import torchvision.datasets as dset
+
+# TODO!!! Directory containing the data.
+#
+root = 'YOUR_PATH_TO_TRAIN_DATA'
+def get_EMNIST(params):
+    #TODO! Part 1. Complete the data_loader and ensure it can successfully load images from your folder
+    transform = transforms.Compose([
+        transforms.Grayscale(),
+        # TODO! Add some processing transforms for data augmentation
+        transforms.ToTensor()])
+
+    # Create the dataset.
+    dataset = dset.ImageFolder(root=root, transform=transform)
+
+    # Create the dataloader.
+    dataloader = torch.utils.data.DataLoader(dataset,
+        batch_size=params['bsize'],
+        shuffle=True)
+
+    return dataloader
