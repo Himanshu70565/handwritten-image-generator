7b1f6ad0fe819bd7dc0d200a97ec130b6be47242
Himanshu <himanshima@myvuw.ac.nz>
2026-09-22 18:33:06 +1200

    fix: correct weight initialization for generator and discriminator class

--- src/dcgan.py
+++ src/dcgan.py
@@ -8,9 +8,9 @@ def weights_init(w):
     Initializes the weights of the layer, w.
     """
     classname = w.__class__.__name__
-    if classname.find('conv') != -1:
+    if classname.find('Conv') != -1:
         nn.init.normal_(w.weight.data, 0.0, 0.02)
-    elif classname.find('bn') != -1:
+    elif classname.find('BatchNorm') != -1:
         nn.init.normal_(w.weight.data, 1.0, 0.02)
         nn.init.constant_(w.bias.data, 0)
 
