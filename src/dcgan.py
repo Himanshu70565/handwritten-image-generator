5b232e572f5840af5e40930dfa15b2a99aef7a36
Himanshu <himanshima@myvuw.ac.nz>
2026-09-24 12:41:19 +1200

    feat: replaced relu with leakyrelu in discriminator stage

--- src/dcgan.py
+++ src/dcgan.py
@@ -87,17 +87,21 @@ class Discriminator(nn.Module):
         # Convolution layer C1: 1 input channel, 64 output channels,
         # 4x4 square convolution, it uses Batch normalization and RELU activation function, and
         # outputs a Tensor with size (N, 64, 16, 16), where N is the size of the batch
-        c1 = F.relu(self.bn1(self.conv1(input)))
+        c1 = F.relu(self.bn1(self.conv1(input)), negative_slope=0.2)
+        # c1 = F.leaky_relu(self.bn1(self.conv1(input)), negative_slope=0.2)
 
         # Convolution layer C2: 64 input channel, 128 output channels,
         # 4x4 square convolution, it uses Batch normalization and RELU activation function, and        
         # outputs a Tensor with size (N, 128, 8, 8), where N is the size of the batch
-        c2 = F.relu(self.bn2(self.conv2(c1)))
+        c2 = F.relu(self.bn2(self.conv2(c1)), negative_slope=0.2)
+        # c2 = F.leaky_relu(self.bn2(self.conv2(c1)), negative_slope=0.2)
+        
         
         # Convolution layer C3: 128 input channel, 256 output channel
         # 4x4 square convolution, it uses Batch normalization and RELU activation function, and
         # outputs a Tensor with size (N, 256, 4, 4), where N is the size of the batch
-        c3 = F.relu(self.bn3(self.conv3(c2)))
+        c3 = F.relu(self.bn3(self.conv3(c2)), negative_slope=0.2)
+        # c3 = F.leaky_relu(self.bn3(self.conv3(c2)), negative_slope=0.2)
 
         # Convolution layer C4: 256 input channel, 1 output channel
         # 4x4 square convolution, it uses Sigmoid activation function, and
